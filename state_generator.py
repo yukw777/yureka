27f3739903d5b6446690251b2e8a894b00fc6e97
Peter Yu <2057325+yukw777@users.noreply.github.com>
2017-12-24 01:35:55 -0500

    Returns positional features. needs testing and clean up

--- state_generator.py
+++ state_generator.py
@@ -1,5 +1,22 @@
 import attr
-from chess import pgn
+import chess
+import chess.pgn
+
+
+pieces = [
+    chess.Piece(chess.PAWN, chess.WHITE),
+    chess.Piece(chess.KNIGHT, chess.WHITE),
+    chess.Piece(chess.BISHOP, chess.WHITE),
+    chess.Piece(chess.ROOK, chess.WHITE),
+    chess.Piece(chess.QUEEN, chess.WHITE),
+    chess.Piece(chess.KING, chess.WHITE),
+    chess.Piece(chess.PAWN, chess.BLACK),
+    chess.Piece(chess.KNIGHT, chess.BLACK),
+    chess.Piece(chess.BISHOP, chess.BLACK),
+    chess.Piece(chess.ROOK, chess.BLACK),
+    chess.Piece(chess.QUEEN, chess.BLACK),
+    chess.Piece(chess.KING, chess.BLACK),
+]
 
 
 @attr.s
@@ -11,11 +28,28 @@ class StateGenerator():
 
     def get_game(self):
         while True:
-            g = pgn.read_game(self.game_file)
+            g = chess.pgn.read_game(self.game_file)
             if g is None:
                 break
             yield g
 
+    def get_square_piece_value(self, piece_map, square, piece):
+        p = piece_map.get(square)
+        if p and p == piece:
+            return 1
+        else:
+            return 0
+
     def generate(self):
         for game in self.get_game():
-            pass
+            board = game.board()
+            piece_map = board.piece_map()
+            for sq, sq_name in enumerate(chess.SQUARE_NAMES):
+                for piece in pieces:
+                    val = self.get_square_piece_value(piece_map, sq, piece)
+                    print(f'{sq_name}-{piece.symbol()}: {val}')
+
+
+if __name__ == '__main__':
+    s = StateGenerator('tests/test.pgn')
+    s.generate()
