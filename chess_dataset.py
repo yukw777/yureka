1a1746c6c95b3dd32942c8fc2778b9ab1864a6fe
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-01-02 21:39:35 -0500

    chess dataset tests done

---
+++ chess_dataset.py
@@ -0,0 +1,58 @@
+import attr
+import chess
+import pandas as pd
+import numpy as np
+import torch
+from state_generator import BOARD_SIZE
+from torch.utils.data import Dataset
+
+
+@attr.s
+class ChessDataset(Dataset):
+    data_file = attr.ib()
+
+    def __attrs_post_init__(self):
+        self.df = pd.read_csv(self.data_file)
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, index):
+        row = self.df.iloc[index]
+        return torch.from_numpy(np.vstack((
+            self.get_board_data(row, bool(row['turn'])),
+            np.full((1, ) + BOARD_SIZE, row['turn']),
+            np.full((1, ) + BOARD_SIZE, row['move_count']),
+            np.full((1, ) + BOARD_SIZE, row['b_kingside_castling']),
+            np.full((1, ) + BOARD_SIZE, row['b_queenside_castling']),
+            np.full((1, ) + BOARD_SIZE, row['w_kingside_castling']),
+            np.full((1, ) + BOARD_SIZE, row['w_queenside_castling']),
+            np.full((1, ) + BOARD_SIZE, row['no_progress']),
+        )))
+
+    def get_square_piece_data(self, data):
+        board_data = np.full(
+            (len(chess.PIECE_TYPES), BOARD_SIZE[0] * BOARD_SIZE[1]), 0)
+
+        for sq_symbol in data.split(','):
+            sq, symbol = sq_symbol.split('-')
+            piece = chess.Piece.from_symbol(symbol)
+            f = chess.FILE_NAMES.index(sq[0])
+            r = chess.RANK_NAMES.index(sq[1])
+            board_data[piece.piece_type - 1][chess.square(f, r)] = 1
+
+        return board_data.reshape((len(chess.PIECE_TYPES), ) + BOARD_SIZE)
+
+    def get_board_data(self, row, turn):
+        white_data = self.get_square_piece_data(row['white_square_piece'])
+        black_data = self.get_square_piece_data(row['black_square_piece'])
+        if turn == chess.WHITE:
+            piece_data = np.vstack((white_data, black_data))
+        else:
+            piece_data = np.vstack((black_data, white_data))
+
+        return np.vstack((
+            piece_data,
+            np.full((1, ) + BOARD_SIZE, row['rep_2']),
+            np.full((1, ) + BOARD_SIZE, row['rep_3']),
+        ))
