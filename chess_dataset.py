251f0835af2a64ee5b4a73918c71e9c7bb0ea8b0
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-01-14 15:58:54 -0500

    move functions in ChessDataset to the module so they could be used in ChessEngine

--- chess_dataset.py
+++ chess_dataset.py
@@ -24,43 +24,49 @@ class ChessDataset(Dataset):
     def __getitem__(self, index):
         row = self.df.iloc[index]
         return (
-            torch.from_numpy(np.vstack((
-                self.get_board_data(row, bool(row['turn'])),
-                np.full(SIZE, row['turn']),
-                np.full(SIZE, row['move_count']),
-                np.full(SIZE, row['b_kingside_castling']),
-                np.full(SIZE, row['b_queenside_castling']),
-                np.full(SIZE, row['w_kingside_castling']),
-                np.full(SIZE, row['w_queenside_castling']),
-                np.full(SIZE, row['no_progress']),
-                np.full(SIZE, 0),
-            ))).float(),
+            get_tensor_from_row(row),
             move_translator.get_engine_move_index(row['move']),
         )
 
-    def get_square_piece_data(self, data):
-        board_data = np.full(
-            (len(chess.PIECE_TYPES), BOARD_SIZE[0] * BOARD_SIZE[1]), 0)
 
-        for sq_symbol in data.split(','):
-            sq, symbol = sq_symbol.split('-')
-            piece = chess.Piece.from_symbol(symbol)
-            square = move_translator.square_name_to_square(sq)
-            board_data[piece.piece_type - chess.PAWN][square] = 1
+def get_tensor_from_row(row):
+    return torch.from_numpy(np.vstack((
+        get_board_data(row, bool(row['turn'])),
+        np.full(SIZE, row['turn']),
+        np.full(SIZE, row['move_count']),
+        np.full(SIZE, row['b_kingside_castling']),
+        np.full(SIZE, row['b_queenside_castling']),
+        np.full(SIZE, row['w_kingside_castling']),
+        np.full(SIZE, row['w_queenside_castling']),
+        np.full(SIZE, row['no_progress']),
+        np.full(SIZE, 0),
+    ))).float()
 
-        return board_data.reshape((len(chess.PIECE_TYPES), ) + BOARD_SIZE)
 
-    def get_board_data(self, row, turn):
-        white_data = self.get_square_piece_data(row['white_square_piece'])
-        black_data = self.get_square_piece_data(row['black_square_piece'])
-        if turn == chess.WHITE:
-            piece_data = np.vstack((white_data, black_data))
-        else:
-            piece_data = np.vstack((black_data, white_data))
+def get_square_piece_data(data):
+    board_data = np.full(
+        (len(chess.PIECE_TYPES), BOARD_SIZE[0] * BOARD_SIZE[1]), 0)
 
-        return np.vstack((
-            piece_data,
-            np.full(SIZE, 1),
-            np.full(SIZE, row['rep_2']),
-            np.full(SIZE, row['rep_3']),
-        ))
+    for sq_symbol in data.split(','):
+        sq, symbol = sq_symbol.split('-')
+        piece = chess.Piece.from_symbol(symbol)
+        square = move_translator.square_name_to_square(sq)
+        board_data[piece.piece_type - chess.PAWN][square] = 1
+
+    return board_data.reshape((len(chess.PIECE_TYPES), ) + BOARD_SIZE)
+
+
+def get_board_data(row, turn):
+    white_data = get_square_piece_data(row['white_square_piece'])
+    black_data = get_square_piece_data(row['black_square_piece'])
+    if turn == chess.WHITE:
+        piece_data = np.vstack((white_data, black_data))
+    else:
+        piece_data = np.vstack((black_data, white_data))
+
+    return np.vstack((
+        piece_data,
+        np.full(SIZE, 1),
+        np.full(SIZE, row['rep_2']),
+        np.full(SIZE, row['rep_3']),
+    ))
