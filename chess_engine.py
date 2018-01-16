382ce3de604dd57c15fea12c78e65131e0ffed57
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-01-15 21:49:46 -0500

    if train mode, return log prob

--- chess_engine.py
+++ chess_engine.py
@@ -4,6 +4,7 @@ import state_generator
 import chess_dataset
 import torch
 from torch.autograd import Variable
+from torch.distributions import Categorical
 from move_translator import (
     translate_to_engine_move,
     translate_from_engine_move,
@@ -17,12 +18,16 @@ class ChessEngine():
     model = attr.ib()
     cuda = attr.ib(default=True)
     transpositions = attr.ib(default=collections.Counter())
+    train = attr.ib(default=True)
 
     def __attrs_post_init__(self):
         self.cuda = self.cuda and torch.cuda.is_available()
         if self.cuda:
             self.model.cuda()
-        self.model.eval()
+        if self.train:
+            self.model.train()
+        else:
+            self.model.eval()
         self.transpositions = collections.Counter()
 
     def get_move(self, board):
@@ -35,19 +40,24 @@ class ChessEngine():
         else:
             inputs = Variable(tensor, volatile=volatile)
         outputs = self.model(inputs)
-        outputs = outputs.view(outputs.shape[0], -1)
-        legal_outputs = filter_illegal_moves(board, outputs)
-        _, move_index = legal_outputs.max(1)
+        probs = outputs.view(outputs.shape[0], -1)
+        self.filter_illegal_moves(board, probs)
+        if self.train:
+            m = Categorical(probs)
+            move_index = m.sample()
+        else:
+            _, move_index = probs.max(1)
         engine_move = get_engine_move_from_index(move_index.data[0])
-        return translate_from_engine_move(engine_move, board.turn)
+        move = translate_from_engine_move(engine_move, board.turn)
+        if self.train:
+            return move, m.log_prob(move_index)
+        else:
+            return move
 
-
-def filter_illegal_moves(board, probs):
-    filtered = Variable(torch.zeros(probs.shape))
-    for move in board.legal_moves:
-        engine_move = translate_to_engine_move(move, board.turn)
-        index = get_engine_move_index(engine_move)
-        filtered.data[0, index] = probs.data[0, index]
-    probs.set_(source=filtered)
-
-    return probs
+    def filter_illegal_moves(self, board, probs):
+        filtered = Variable(torch.zeros(probs.shape))
+        for move in board.legal_moves:
+            engine_move = translate_to_engine_move(move, board.turn)
+            index = get_engine_move_index(engine_move)
+            filtered.data[0, index] = probs.data[0, index]
+        probs.set_(source=filtered)
