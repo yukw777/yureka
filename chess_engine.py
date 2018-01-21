50a8d6ace7c65c198c3f64fd89adc5c78269d0bc
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-01-21 17:45:40 -0500

    Chess Engine UCI communication

--- chess_engine.py
+++ chess_engine.py
@@ -1,4 +1,9 @@
+#!/home/keunwoo/Documents/Projects/chess-engine/venv/bin/python
+
 import attr
+import models
+import sys
+import os
 import collections
 import chess
 import state_generator
@@ -100,3 +105,100 @@ def queen_promotion_if_possible(board, move):
         move.promotion = chess.QUEEN
         return move
     return move
+
+
+@attr.s
+class UCI():
+    model = attr.ib()
+    model_file = attr.ib()
+
+    def __attrs_post_init__(self):
+        self.handlers = {
+            'uci': self.uci,
+            'isready': self.isready,
+            'ucinewgame': self.ucinewgame,
+            'position': self.position,
+            'go': self.go,
+            'quit': self.quit,
+        }
+        self.model = models.create(self.model)
+        self.model.load_state_dict(torch.load(self.model_file))
+        self.init_engine()
+
+    def init_engine(self):
+        self.engine = ChessEngine(self.model, train=False)
+        self.board = chess.Board()
+
+    def uci(self, args):
+        print('id name Yureka 0.1')
+        print('id author Peter Yu')
+        print('uciok')
+
+    def isready(self, args):
+        print('readyok')
+
+    def ucinewgame(self, args):
+        self.init_engine()
+
+    def position(self, args):
+        args = args.split()
+        if args[0] == 'startpos':
+            fen = chess.STARTING_FEN
+            moves = args[2:]
+        elif args[0] == 'fen':
+            fen = ' '.join(args[1:7])
+            moves = args[7:]
+        else:
+            self.unknown_handler(' '.join(args))
+            return
+        self.board = chess.Board(fen=fen)
+        for uci in moves:
+            self.board.push_uci(uci)
+
+    def go(self, args):
+        move = self.engine.get_move(self.board)
+        print(f'bestmove {move.uci()}')
+
+    def quit(self, args):
+        sys.exit()
+
+    def unknown_handler(self, command):
+        print(f'Unknown command: {command}')
+
+    def parse_command(self, raw_command):
+        parts = raw_command.split(maxsplit=1)
+        if len(parts) == 1:
+            args = ''
+        else:
+            args = parts[1]
+        return parts[0], args
+
+    def handle(self, raw_command):
+        command, args = self.parse_command(raw_command)
+        h = self.handlers.get(command)
+        if not h:
+            self.unknown_handler(raw_command)
+        else:
+            h(args)
+
+    def listen(self):
+        while True:
+            command = input()
+            self.handle(command)
+
+
+if __name__ == '__main__':
+    import argparse
+    default_model = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'saved_models',
+        '2018-01-08',
+        'ChessEngine_2018-01-09_22:26:13_11.model',
+    )
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', default='ChessEngine.v0')
+    parser.add_argument('-f', '--model-file', default=default_model)
+
+    args = parser.parse_args()
+    uci = UCI(args.model, args.model_file)
+    uci.listen()
