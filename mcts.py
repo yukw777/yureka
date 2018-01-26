fc85cde8a7cbd39a2152b985d45451ad27764081
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-01-25 23:33:06 -0500

    Node for MCTS done with tests.

---
+++ mcts.py
@@ -0,0 +1,55 @@
+import attr
+import chess
+import math
+import collections
+from board_data import get_board_data
+
+
+@attr.s
+class Node():
+    children = attr.ib(default={})
+    prior = attr.ib(default=0)
+    result = attr.ib(default=0)
+    value = attr.ib(default=0)
+    visit = attr.ib(default=0)
+    board = attr.ib(default=chess.Board())
+    transpositions = attr.ib(default=collections.Counter())
+    lambda_c = attr.ib(default=0.5)
+    confidence = attr.ib(default=1)
+
+    def __attrs_post_init__(self):
+        self.board_data = get_board_data(self.board, self.transpositions)
+
+    @property
+    def q(self):
+        q = (1 - self.lambda_c) * self.value / self.visit
+        q += self.lambda_c * self.result / self.visit
+        return q
+
+    def ucb(self, visit_sum):
+        # alpha go version
+        ucb = self.q
+        ucb += self.confidence * math.sqrt(visit_sum) / (1 + self.visit)
+        return ucb
+
+    def add_child(self, move, **kwargs):
+        b = chess.Board(fen=self.board.fen())
+        b.push(move)
+        self.children[move] = Node(
+            board=b,
+            transpositions=self.transpositions,
+            **kwargs
+        )
+
+
+@attr.s
+class MCTS():
+    root = attr.ib()
+    rollout = attr.ib()
+    value = attr.ib()
+    terminate_search = attr.ib()
+
+    def search(self):
+        while not self.terminate_search():
+            self.select()
+            self.expand()
---
+++ tests/mcts_test.py
@@ -0,0 +1,55 @@
+import mcts
+import chess
+
+
+def test_node_calculations():
+    test_cases = [
+        {
+            'lambda': 0.1,
+            'value': 0.7,
+            'visit': 4,
+            'result': 1,
+            'confidence': 4,
+            'expected_q': 0.1825,
+            'expected_ucb': 8.1825,
+        },
+        {
+            'lambda': 0.9,
+            'value': -0.5,
+            'visit': 5,
+            'result': -1,
+            'confidence': 6,
+            'expected_q': -0.19,
+            'expected_ucb': 9.81,
+        },
+    ]
+
+    for tc in test_cases:
+        n = mcts.Node(
+            lambda_c=tc['lambda'],
+            value=tc['value'],
+            visit=tc['visit'],
+            result=tc['result'],
+            confidence=tc['confidence'],
+        )
+        assert n.q == tc['expected_q']
+        assert n.ucb(100) == tc['expected_ucb']
+
+
+def test_node_add_child():
+    root = mcts.Node()
+    assert root.children == {}
+    root.add_child(chess.Move.from_uci('a2a4'), prior=0.5)
+    root.add_child(chess.Move.from_uci('b2b4'), prior=0.3)
+
+    child1 = root.children[chess.Move.from_uci('a2a4')]
+    child2 = root.children[chess.Move.from_uci('b2b4')]
+    assert child1.prior == 0.5
+    assert child2.prior == 0.3
+
+    b = chess.Board()
+    b.push_uci('a2a4')
+    assert child1.board == b
+    b = chess.Board()
+    b.push_uci('b2b4')
+    assert child2.board == b
