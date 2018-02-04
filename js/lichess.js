246438589c3dc35d621452ea3aac6aa4d863230e
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-02-03 21:42:21 -0500

    get color and handle catching up

--- js/lichess.js
+++ js/lichess.js
@@ -42,26 +42,53 @@ class LichessClient {
         })
         .then(function (engine) {
             console.log('Engine is ready');
-            console.log('Connecting to the WebSocket...');
-            const ws = new websocket(socketUrl, null, {
-                headers: {
-                    'Cookie': this.cookie,
+            console.log('Fetching game information...');
+            axios.get('https://lichess.org/api/game/' + game)
+            .then(function(response) {
+                var color;
+                if (response.data.players.black.userId === this.username) {
+                    color = 'black';
+                } else if (response.data.players.white.userId === this.username){
+                    color = 'white';
                 }
-            });
-            ws.on('open', () => {
-                setInterval(() => {
-                    const data = JSON.stringify({t: 'p', v: socketVersion});
-                    console.log('Sending: ' + data);
-                    ws.send(data);
-                }, 1000);
-            });
-            ws.on('message', data => {
-                console.log('Received: ' + data);
-            });
-            ws.on('error', error => {
-                console.log(error);
-            });
-        }.bind(this));
+                console.log('My color is ' + color);
+                return Promise.resolve(color);
+            }.bind(this))
+            .then(function(color) {
+                console.log('Connecting to the WebSocket...');
+                const ws = new websocket(socketUrl, null, {
+                    headers: {
+                        'Cookie': this.cookie,
+                    }
+                });
+                ws.on('open', () => {
+                    setInterval(() => {
+                        const data = JSON.stringify({t: 'p', v: socketVersion});
+                        console.log('Sending: ' + data);
+                        ws.send(data);
+                    }, 1000);
+                });
+                ws.on('message', data => {
+                    console.log('Received: ' + data);
+                    const parsed = JSON.parse(data);
+                    switch (parsed.t) {
+                        case 'move':
+                            break;
+                        case 'b':
+                            // catch up
+                            // get the last frame
+                            const last = parsed.d[parsed.d.length - 1];
+                            socketVersion = last.v;
+                            break;
+                    }
+                });
+                ws.on('error', error => {
+                    console.log(error);
+                });
+            }.bind(this))
+            .catch(console.error);
+        }.bind(this))
+        .catch(console.error);
         //.position('startpos')
         //.go({movetime: 10000})
         //.then(result => {
