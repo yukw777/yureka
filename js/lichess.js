14b77d5a09a0f85ca00d34d63fb16e9c0a9585a0
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-04-23 23:10:25 -0400

    Remove the js integration with lichess. Use the official client instead

--- js/lichess.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const LichessClient = require('./lichess-client');
-
-const c = new LichessClient(process.argv[2], process.argv[3]);
-c.login();
-c.play(process.argv[4], process.argv[5]);
