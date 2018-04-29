fa1fdbcee8455412c184cd7d11a42fa6e3bf4e74
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-04-29 15:53:02 -0400

    better variable name

--- yureka/mcts.py
+++ yureka/mcts.py
@@ -287,21 +287,21 @@ class RandomPolicy():
 
 @attr.s
 class ValueNetwork():
-    value = attr.ib()
+    network = attr.ib()
     cuda = attr.ib(default=True)
     cuda_device = attr.ib(default=None)
 
     def __attrs_post_init__(self):
-        self.value.eval()
+        self.network.eval()
         self.cuda = self.cuda and torch.cuda.is_available()
         if self.cuda:
-            self.value.cuda(self.cuda_device)
+            self.network.cuda(self.cuda_device)
 
     def get_value(self, board):
         board_data = get_board_data(board)
         tensor = chess_dataset.get_tensor_from_row(board_data)
         tensor = tensor.unsqueeze(0)
-        value = self.value(Variable(tensor.cuda(), volatile=True))
+        value = self.network(Variable(tensor.cuda(), volatile=True))
         return value.squeeze().data[0]
 
 
