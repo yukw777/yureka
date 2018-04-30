77de6820a4c69a82b2945cf219c5d863b0c1f37c
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-04-29 21:03:36 -0400

    Move ZeroValue and RandomPolicy

--- yureka/mcts/networks.py
+++ yureka/mcts/networks.py
@@ -1,9 +1,15 @@
 import attr
 import torch
+import random
 
 from torch.autograd import Variable
 
 from yureka.board_data import get_board_data
+from yureka.move_translator import (
+    TOTAL_MOVES,
+    translate_to_engine_move,
+    get_engine_move_index,
+)
 from yureka import chess_dataset
 
 
@@ -25,3 +31,25 @@ class ValueNetwork():
         tensor = tensor.unsqueeze(0)
         value = self.network(Variable(tensor.cuda(), volatile=True))
         return value.squeeze().data[0]
+
+
+class ZeroValue():
+    def get_value(self, board):
+        return 0
+
+
+class RandomPolicy():
+    def get_move(self, board, sample=True):
+        return random.choice(list(board.legal_moves))
+
+    def get_probs(self, board):
+        moves = list(board.legal_moves)
+        prob = 1/len(moves)
+        probs = Variable(torch.zeros(1, TOTAL_MOVES))
+        indexes = []
+        for move in moves:
+            engine_move = translate_to_engine_move(move, board.turn)
+            index = get_engine_move_index(engine_move)
+            indexes.append(index)
+        probs.index_fill_(1, Variable(torch.LongTensor(indexes)), prob)
+        return probs
--- yureka/yureka.py
+++ yureka/yureka.py
@@ -3,11 +3,9 @@
 import attr
 import chess
 import torch
-from torch.autograd import Variable
-import random
 import os
 import sys
-from .mcts.networks import ValueNetwork
+from .mcts.networks import ValueNetwork, ZeroValue, RandomPolicy
 from .mcts import Node, MCTS
 from .engine.time_manager import TimeManager
 from .mcts.constants import DEFAULT_CONFIDENCE
@@ -16,11 +14,6 @@ from .chess_engine import (
     print_flush,
     ChessEngine,
     UCIEngine,
-)
-from .move_translator import (
-    TOTAL_MOVES,
-    translate_to_engine_move,
-    get_engine_move_index,
 )
 
 
@@ -45,28 +38,6 @@ DEFAULT_POLICY_FILE = os.path.join(
     'SL_endgame',
     'Policy_2018-01-27_07:09:34_14.model',
 )
-
-
-class ZeroValue():
-    def get_value(self, board):
-        return 0
-
-
-class RandomPolicy():
-    def get_move(self, board, sample=True):
-        return random.choice(list(board.legal_moves))
-
-    def get_probs(self, board):
-        moves = list(board.legal_moves)
-        prob = 1/len(moves)
-        probs = Variable(torch.zeros(1, TOTAL_MOVES))
-        indexes = []
-        for move in moves:
-            engine_move = translate_to_engine_move(move, board.turn)
-            index = get_engine_move_index(engine_move)
-            indexes.append(index)
-        probs.index_fill_(1, Variable(torch.LongTensor(indexes)), prob)
-        return probs
 
 
 @attr.s
