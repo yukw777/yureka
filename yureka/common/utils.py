0493faa246f06a7e64134c27c81ec2152aa5d2d3
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-06-13 17:13:01 -0400

    Revert "ForkablePdb for debugging multiprocesses"
    This reverts commit 87f71e2db923af3b51787c8cae1af582048d6115.

--- yureka/common/utils.py
+++ yureka/common/utils.py
@@ -1,26 +1,4 @@
 import time
-import pdb
-import sys
-import os
-
-
-class ForkablePdb(pdb.Pdb):
-
-    _original_stdin_fd = sys.stdin.fileno()
-    _original_stdin = None
-
-    def __init__(self):
-        pdb.Pdb.__init__(self, nosigint=True)
-
-    def _cmdloop(self):
-        current_stdin = sys.stdin
-        try:
-            if not self._original_stdin:
-                self._original_stdin = os.fdopen(self._original_stdin_fd)
-            sys.stdin = self._original_stdin
-            self.cmdloop()
-        finally:
-            sys.stdin = current_stdin
 
 
 def timeit(method):
