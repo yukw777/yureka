2321df2e15fe73bcf1a79f4aaed81ecac183de23
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-06-14 11:30:06 -0400

    deeper resnet

--- yureka/learn/models/__init__.py
+++ yureka/learn/models/__init__.py
@@ -71,6 +71,21 @@ resnet_settings = {
         'value_hidden': 128,
         'value_out_channels': 1,
     },
+    'ResNet.v1': {
+        'in_channels': 21,
+        'conv_block_out_channels': 256,
+        'conv_block_kernel': 3,
+        'conv_block_padding': 1,
+        'conv_block_stride': 1,
+        'res_block_out_channels': 256,
+        'res_block_kernel': 3,
+        'res_block_padding': 1,
+        'res_block_stride': 1,
+        'res_blocks': 10,
+        'policy_out_channels': 128,
+        'value_hidden': 256,
+        'value_out_channels': 1,
+    },
 }
 
 
--- yureka/tests/learn/models/res_test.py
+++ yureka/tests/learn/models/res_test.py
@@ -14,3 +14,16 @@ def test_res_v0():
     # (batch_size, 1)
     value_output = value(tower(input))
     assert value_output.shape == (16, 1)
+
+
+def test_res_v1():
+    tower, policy, value = models.create('ResNet.v1')
+    # (batch_size, in_channels, 8, 8)
+    input = torch.randn(16, 21, 8, 8)
+    # (batch_size, num_move_planes * 8 * 8)
+    policy_output = policy(tower(input))
+    assert policy_output.shape == (16, NUM_MOVE_PLANES * 8 * 8)
+
+    # (batch_size, 1)
+    value_output = value(tower(input))
+    assert value_output.shape == (16, 1)
