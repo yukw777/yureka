6b3a29004f4f0940367cd80c5da2779fb0fd1639
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-06-17 02:42:44 -0400

    decrease block number on ResNet.v0 to combat overfitting

--- yureka/learn/models/__init__.py
+++ yureka/learn/models/__init__.py
@@ -67,7 +67,7 @@ resnet_settings = {
         'res_block_kernel': 3,
         'res_block_padding': 1,
         'res_block_stride': 1,
-        'res_blocks': 5,
+        'res_blocks': 3,
         'policy_out_channels': 128,
         'value_hidden': 128,
         'value_out_channels': 1,
